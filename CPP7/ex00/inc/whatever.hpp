fdb82c22941f6550adeec5b5f3b0e6b3c0c1cdf4
deo <>
2024-10-25 22:08:21 +0200

    feat: CPP7 start ex00 complete

---
+++ CPP7/ex00/inc/whatever.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: droied <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 21:45:50 by droied            #+#    #+#             */
+/*   Updated: 2024/10/25 22:05:26 by droied           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+
+template <typename T> void swap(T &a, T &b);
+template <typename T> T min(T a, T b);
+template <typename T> T max(T a, T b);
+
+#endif //WHATEVER_HPP
---
+++ CPP7/ex01/inc/iter.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: droied <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 21:45:50 by droied            #+#    #+#             */
+/*   Updated: 2024/10/25 22:07:26 by droied           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+
+// template <typename T> void swap(T &a, T &b);
+// template <typename T> T min(T a, T b);
+// template <typename T> T max(T a, T b);
+
+#endif //ITER_HPP
