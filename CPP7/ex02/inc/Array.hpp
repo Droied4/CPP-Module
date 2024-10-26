e8fed509f22a8c8e81e947f6cc6477e9a880a1a2
deo <>
2024-10-26 12:05:14 +0200

    feat: ex02 init

---
+++ CPP7/ex02/inc/Array.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: droied <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 11:45:07 by droied            #+#    #+#             */
+/*   Updated: 2024/10/26 11:57:52 by droied           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+template<typename T>
+
+class Array
+{
+	private:
+		T	m_array[];
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(const Array &t_obj);
+		Array &operator=(const Array &t_obj);
+		~Array();
+		
+		Array &operator[](unsigned int n); //• Elements can be accessed through the subscript operator: [ ]. creo que es asi
+		unsigned int size(); //• returns the number of elements in the array
+};
+
+#endif //ARRAY_HPP
