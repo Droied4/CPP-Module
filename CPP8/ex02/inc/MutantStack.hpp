15b81111a24c04471f8c0c357ab0a756d04f7ef8
Deivid Santiago Ordoñez Varela <deordone@car12s1.42barcelona.com>
2024-11-05 11:55:17 +0100

    feat: CPP8 ex02 init

---
+++ CPP8/ex02/inc/MutantStack.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deordone <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 11:53:14 by deordone          #+#    #+#             */
+/*   Updated: 2024/11/05 11:54:40 by deordone         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+
+class MutantStack
+{
+	private:
+
+	public:
+
+};
+
+#endif //MUTANTSTACK_HPP
