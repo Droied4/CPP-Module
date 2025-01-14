c9e34b8295e7375f93bd1ce1e1fc40c2d2f97827
droied <deiord44santiagoov@gmail.com>
2025-01-14 18:59:20 +0100

    better

--- CPP9/ex00/src/BitcoinExchange.cpp
+++ CPP9/ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: droied <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 21:25:22 by droied            #+#    #+#             */
-/*   Updated: 2025/01/14 13:24:45 by deordone         ###   ########.fr       */
+/*   Updated: 2025/01/14 18:58:51 by droied           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,35 +126,63 @@ bool BitcoinExchange::checkFile(std::deque<std::string> &dq_out, unsigned long i
 	return (false);
 }
 
-int getConversion(std::string date_target, std::string date_match)
+void trimDate(std::string date, int &year, int &month, int &day)
 {
-	//aqui lo que tienes que hacer es hacer un trim de date_target y de date_match
-	//si coinciden en el ano entonces avanzas al siguiente sino restas (se para en 0)
-	//si coinciden en el mes entonces avanzas al siguiente sino restas (se para en 0)
-	//si coinciden en el dia entonces ya tienes la fecha sino restas (se para en 0)
+	std::string::size_type line = date.find("-");
+	std::istringstream(date.substr(0, line)) >> year;
+
+	date.erase(0, line + 1);
+	line = date.find("-");
+	std::istringstream(date.substr(0, line)) >> month;
+
+	date.erase(0, line + 1);
+	std::istringstream(date) >> day;
+}
+
+bool compareDates(int year, int month, int day, std::string date_match)
+{
+	int year_match(0);
+	int month_match(0);
+	int day_match(0);
+
+	trimDate(date_match, year_match, month_match, day_match);
+	if (year_match >= year)
+	{
+		if (month_match >= month)
+			return (day_match <= day);
+	}
+	//va por ahi mas o menos ahora necesito entender por que el 2025 no lo lee
+	return (true);
 }
 
 void BitcoinExchange::conversion(std::deque<std::string> &dq_out, unsigned long i)
 {
+	(void)dq_out;	
 	//get the date target
 	std::string dq_in = m_dq_in[i];
 	std::string::size_type pipe = dq_in.find("|");
 	std::string date_target = dq_in.substr(0, pipe - 1);
 	//trim date
-	int year(0);
-	int month(0);
-	int day(0);
+	int year_target(0);
+	int month_target(0);
+	int day_target(0);
 
-	std::string::size_type line = date_target.find("-");
-	std::istringstream(date_target.substr(0, line)) >> year;
-
-	date.erase(0, line + 1);
-	line = date.find("-");
-	std::istringstream(date.substr(0, line)) >> month;
+	trimDate(date_target, year_target, month_target, day_target);
 	
-	date.erase(0, line + 1);
-	std::istringstream(date) >> day;
-
+	std::deque<std::string>::iterator it(m_dq_database.begin());
+	while (it != m_dq_database.end())
+	{
+		std::string db_line = *it;
+		std::string::size_type coma = db_line.find(",");
+		std::string db_date = db_line.substr(0, coma);
+		if (!compareDates(year_target, month_target, day_target, db_date))
+		{
+			it--;
+			break ;
+		}
+		it++;
+	}
+	std::cout << *it << "\n";
 	// no se como mierda hacerlo osea, necesito conseguir el ano comparando en la base de datos con todo pero tengo que hacer un trim del ano y tambien si no coincide.
 }
 
