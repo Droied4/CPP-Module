0921386e61847e304c0f34af8380eefabc14e29b
Deivid Santiago Ordoñez Varela <deordone@cbr14s2.42barcelona.com>
2025-01-25 02:19:17 +0100

    setting up

---
+++ CPP9/ex02/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deordone <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/25 02:16:44 by deordone          #+#    #+#             */
+/*   Updated: 2025/01/25 02:18:54 by deordone         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main (int ac, char *av[])
+{
+	(void)av;
+	if (ac <= 3000)
+	{
+		printf("see\n");
+	}
+	else
+	{
+		printf("Your program must be able to handle at least 3000 different integers.\n That's right i'm lazy as fck");
+	}
+	return (0);
+}
