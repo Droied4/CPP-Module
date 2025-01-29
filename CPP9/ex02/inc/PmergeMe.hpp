0359de3ff138e1151df5c5d0603b82dfc440b516
deo <>
2025-01-29 18:10:05 +0100

    no se en que porongas me meti te maldigo cpp9 ex03

--- CPP9/ex02/inc/PmergeMe.hpp
+++ CPP9/ex02/inc/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: droied <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 11:26:34 by droied            #+#    #+#             */
-/*   Updated: 2025/01/29 17:33:08 by droied           ###   ########.fr       */
+/*   Updated: 2025/01/29 18:08:48 by droied           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ typedef std::multimap<unsigned int, unsigned int> mmap;
 class PmergeMe 
 {
 	private:
+		template <typename T> void swap(T &c, typename T::iterator pos);
 	public:
 		PmergeMe();
 		PmergeMe(const PmergeMe &obj);
@@ -32,11 +33,40 @@ class PmergeMe
 		~PmergeMe();
 
 		template <typename T> void insertValue(std::string input, T &container);
-		
+		template <typename T> void part1(T &c);	
 
 		//aux
 		template <typename T> void print(T &a);
 };
+
+template <typename T> 
+void swap(T &c, typename T::iterator pos)
+{
+	
+}
+
+template <typename T> 
+void part1(T &c)
+{
+	typedef typename T::iterator iterator;
+	unsigned int max_pairs(0);
+	max_pairs = c.size() / 2;
+	if (max_pairs < 2)
+		return ;
+
+	iterator it(c.begin());
+	for (unsigned int i(0); i < max_pairs; i++)
+	{
+		if (*it > *(it++))
+			swap(c, it);
+		it += 2;
+	}
+
+	//la parte 1 se divide en dos cosas
+	//1 dividir elementos en pares
+	//2 sortear los pares
+	//esto se debe hacer hasta que ya no se puedan hacer mas pares
+}
 
 template <typename T> 
 void PmergeMe::insertValue(std::string input, T &container)
