2cd06bc8ab6d2998ed0f69285bb9f06f47e84c7c
droied <deiord44santiagoov@gmail.com>
2025-02-06 16:19:40 +0100

    p2 advance 1.1

--- CPP9/ex02/inc/PmergeMe.hpp
+++ CPP9/ex02/inc/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: droied <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 11:26:34 by droied            #+#    #+#             */
-/*   Updated: 2025/02/06 13:38:53 by droied           ###   ########.fr       */
+/*   Updated: 2025/02/06 15:09:38 by droied           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ class PmergeMe
 		template <typename T> void my_swap(T &c, typename T::iterator pos, int recursion_lvl);	
 		template <typename T> typename T::iterator my_prev(typename T::iterator pos, int recursion_lvl);	
 		template <typename T> int pairSort(T &c, int recursion_lvl);	
+		template <typename T> void insertPend(T &c, T &pend);	
 		template <typename T> void binaryInsertion(T &c, int recursion_lvl);	
 
 		//aux
@@ -58,14 +59,18 @@ void PmergeMe::my_swap(T &c, typename T::iterator pos, int recursion_lvl)
 	typedef typename T::iterator iterator;
 	iterator prev;
 	iterator curr = pos;
-	prev = my_prev<T>(curr, recursion_lvl - (recursion_lvl >> 1));
-	for (int a(0); a < recursion_lvl - (recursion_lvl >> 1); a++)
+	prev = my_prev<T>(curr, (recursion_lvl >> 1));
+	for (int a(0); a < (recursion_lvl >> 1); a++)
 	{
 		std::swap(curr->second, prev->second);
 		curr--;
 		prev--;
 	}
 }
+
+// template <typename T>
+// void PmergeMe::insertPend(T &c, T &c)
+// {}
 
 template <typename T>
 void PmergeMe::binaryInsertion(T &c, int recursion_lvl)
@@ -75,17 +80,29 @@ void PmergeMe::binaryInsertion(T &c, int recursion_lvl)
 		return ;
 	iterator curr(c.begin());
 	iterator prev;
+	iterator end = c.end();
+	end--;
 	for (int a(1); a < recursion_lvl; a++)
 		curr++;
-	prev = my_prev<T>(curr, recursion_lvl - (recursion_lvl >> 1));
-
-
+	int odd(1);
+	//iteracion mal no deberia agregar el 12 
+	do
+	{
+		prev = my_prev<T>(curr, (recursion_lvl >> 1));
+		if (odd != 1 && odd % 2 != 0)
+			std::cout << "insert this -> " << prev->second << "\n";
+			// insertPend(c, pend);
+		// std::cout << "Current -> "<< curr->second << "\n";
+		// std::cout << "Prev -> "<< prev->second << "\n";
+		for (int a(0); a < recursion_lvl; a++)
+			curr++;
+		odd += 2;
+	}
+	while(curr != end++);
 	//while
 	// insertPend(c, pend); //function insert on pend odd elements. except 1
 	// binarySearch(c, pend); //binary search and insert the value in the corresponding place.
 
-	std::cout << "Current -> "<< curr->second << "\n";
-	std::cout << "Prev -> "<< prev->second << "\n";
 	binaryInsertion(c, recursion_lvl >> 1);
 }
 
@@ -103,7 +120,7 @@ int PmergeMe::pairSort(T &c, int recursion_lvl)
 	end--;
 	do 
 	{
-		prev = my_prev<T>(i, recursion_lvl - (recursion_lvl >> 1));
+		prev = my_prev<T>(i, (recursion_lvl >> 1));
 		if (prev->second > i->second)
 			my_swap(c, i, recursion_lvl);
 		for (int a(0); a < recursion_lvl; a++)
